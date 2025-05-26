e1c8815522ec77d84cda752ce8eea87b8d831711
Michael <themichaelsteve@gmail.com>
2025-05-26 14:40:32 +0100

    6. Advanced Task

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,4 +1,5 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button"
 
 
 const Landing: React.FC =  () => {
@@ -6,7 +7,13 @@ const Landing: React.FC =  () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+      <Button title="Small Button" styles={{ size: "small", shape: "rounded-full" }} />
+
       <Card />
+      <Button title="Medium Button" styles={{ size: "medium" }} />
+
+      <Card />
+      <Button title="Large Button" styles={{ size: "large", shape: "rounded-sm" }} />
     </div>
   )
 }
